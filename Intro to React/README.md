752142478223b15d4d91b9d47430b2239de9d867
kiannadalton <kiannadalton@aol.com>
2025-09-22 12:39:42 -0400

    Completed Intro to React Workshop and added deployed link in README.md within Intro to React folder

--- Intro to React/README.md
+++ Intro to React/README.md
@@ -1,9 +1,14 @@
 # Intro to React
 
 This is the first assignment where React is utilized in conjunction with Vite.
+Deployed React application: puppypalsassignment.netlify.app
 
 ## Learning Lessons:
 
+The biggest learning lesson was understanding how useState works, especially how it relates to ternary operators. as I was going through this assignment, I found an error in the instructions. They stated that the ternary should be tied to the setFeatPupId. This caused issues when the page was initially rendered since it was considered always true. As a result, there would be an error as featuredPup tried to find data on 'undefined'. I fixed this by trading out setFeatPupId with featPupId and this solved the issue.
+
+After further looking into the useState hook, I noted that the parameters are [(currentValue), (functionToChangeValue)]. We'd want to check the boolean based on the currentValue, and not the paramater that changes is the value.
+
 ## Demonstrates use of:
 
-.map
+useState, onClick, .map, .find, ternary operator
--- Intro to React/Workshop/Puppy_Pals/src/App.jsx
+++ Intro to React/Workshop/Puppy_Pals/src/App.jsx
@@ -1,12 +1,49 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { puppyList } from "./data";
 
 function App() {
+  //puppyList is the default value. puppies is the value I'm storing, setPuppues is a function I can use to reset the value (puppies).
+  const [puppies, setPuppies] = useState(puppyList);
+  // setting default value to null helps let me conditionally render this
+  const [featPupId, setFeatPupId] = useState(null);
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
 
   return (
     <div>
-
+      <h1>Puppy Pals</h1>
+      {/* This a shortcut to conditionally render. If featPupId is true, it renders the right code. If it's not true, it's considered false and renders nothing. */}{" "}
+      {featPupId ? (
+        <div className="singlePup">
+          <h3>Name: {featuredPup.name}</h3>
+          <p>Email: {featuredPup.email}</p>
+          <p>
+            Are they cute?{" "}
+            {featuredPup.isCute ? <span>Yes</span> : <span>No</span>}
+          </p>
+          <p>Age: {featuredPup.age}</p>
+        </div>
+      ) : (
+        <p>Click on a puppy to see more!</p>
+      )}
+      <h2>Available Puppies:</h2>
+      <div className="allPups">
+        {puppies.map((puppy) => {
+          return (
+            <p
+              className="puppyName"
+              onClick={() => {
+                setFeatPupId(puppy.id);
+              }}
+              key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
